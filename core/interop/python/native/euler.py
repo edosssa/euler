b19920aec48816340f815f278a1fe869635afab0
Edosa Kelvin <edosakelvin@gmail.com>
2019-06-28 19:58:50 +0100

    Refactored euler python lib to target python 3.7.3

--- core/interop/python/native/euler.py
+++ core/interop/python/native/euler.py
@@ -7,7 +7,7 @@ import json
 import struct
 import sys
 import threading
-import Queue
+import queue
 
 # On Windows, the default I/O mode is O_TEXT. Set this to O_BINARY
 # to avoid unwanted modifications of the input/output streams.
@@ -17,40 +17,33 @@ if sys.platform == "win32":
     msvcrt.setmode(sys.stdin.fileno(), os.O_BINARY)
     msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)
 
-
-################################################################
-# IPC helper functions for communincating with the euler host. #
-################################################################
-
 def send_message(message):
    # Write message size.
-    sys.stdout.write(struct.pack('I', len(message)))
+    # sys.stdout.write(struct.pack('I', len(message)))
+    sys.stdout.buffer.write(struct.pack('I', len(message)))
+
     # Write the message itself.
-    sys.stdout.write(message)
+    sys.stdout.buffer.write(message.encode('utf-8'))
     sys.stdout.flush()
 
 def read_message():
     # Read the message length (first 4 bytes).
-    text_length_bytes = sys.stdin.read(4)
+    text_length_bytes = sys.stdin.buffer.read(4)
     if len(text_length_bytes) == 0:
         sys.exit(0)
     # Unpack message length as 4 byte integer.
     text_length = struct.unpack('i', text_length_bytes)[0]
     # Read the text (JSON object) of the message.
-    text = sys.stdin.read(text_length).decode('utf-8')
+    text = sys.stdin.buffer.read(text_length).decode('utf-8')
     return text
 
-
-###############################
-# Euler STD wrapper functions #
-###############################
+################################################################
 
 def write(text):
     req = {"cmd": "print", "args": [text]}
     send_message(json.dumps(req))
     # read the response back, but discard it
     read_message()
-
 
 def mean(array):
     request = {"cmd": "mean", "args": [array]}
